4c01c07776ac0c397544c15aeff31243cfbf4067
Rodrigo Veiga <rodrigo.veiga@grupoccaa.com.br>
2025-01-05 10:41:37 -0300

    Laravel: criando uma aplicação com MVC (Adaptado para Laravel 11)

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Serie;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        //$series = Serie::all();
+        $series = Serie::query()->orderBy('nome')->get();
+
+        //dd($series);
+
+        return view('series.index',compact('series'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        return redirect('/series');
+
+    }
+}
---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,10 @@
+<x-layout title="Nova Série">
+    <form action="/series/salvar" method="post">
+        @csrf
+        <div class="mb-3">
+            <label class="form-label" for="nome">Nome:</label>
+            <input class="form-control" type="text" id="nome" name="nome">
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
+</x-layout>
